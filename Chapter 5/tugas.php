d35b4b57238cf64209a535052604f5337273738c
GusniarNanda <127200033+GusniarNanda@users.noreply.github.com>
2023-06-05 07:36:46 +0700

    Add files via upload

---
+++ Chapter 5/tugas.php
@@ -0,0 +1,17 @@
+
+<?php
+$bd = "<b>BIODATA SAYA ADALAH SEBAGAI BERIKUT</b>";
+$nl = "Andi Firmansyah";
+$TTL = "Cilacap, 26 April 1991";
+$almt = "Jalan Kalimantan, Cilacap";
+$ag = "Islam";
+$pk = "Mahasiswa";
+$st = "Perjaka";
+echo " " . $bd . "<br>";
+echo "Nama Lengkap  :" . $nl. "<br>";
+echo "Tempat/Tanggal Lahir  :" . $TTL . "<br>";
+echo "Alamat    :" . $almt . "<br>";
+echo "Agama :" . $ag . "<br>";
+echo "Pekerjaan" . $pk . "<br>";
+echo "Status" . $st . "<br>";
+?>
